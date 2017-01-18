4fcb66517a3e836f1d2d60cc1eb77eaa903bcf12
Tony Weidel <8aweidel@stpatrick.org>
2017-01-18 14:03:32 -0600

    I did close to nothing.  Thanks, dead computer.

---
+++ mymatrix.js
@@ -0,0 +1,11 @@
+//Imports matrix code from the folder
+const Matrix = require("matrix-js");
+
+/*Example code creating two new matricies and then multiplying them.*/
+let M = Matrix([[1,1],[1,1]]);
+let N = Matrix([[2,3],[3,5]]);
+//console.log(M.prod(N));
+
+//Draft your code below this line to answer the questions.
+//You can 'comment out' theexample above by placing double slashes before them.
+let A = Matrix([[13,9,20],[3,8,]])
